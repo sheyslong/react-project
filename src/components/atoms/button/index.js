1e8e6ba38f90cc76e7aa4103c8b8429fa7a5267c
anderson.f.vidal <anderson.f.vidal@accenture.com>
2020-09-20 15:05:15 -0300

    rollback fonts

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ButtonAtom } from './styled';
-import './index.css'
 
 export const Button = ({...props}) => {
   return <ButtonAtom shape="omitted" {...props}/>;
