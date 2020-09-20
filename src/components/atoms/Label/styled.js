4d1a509e0c4f42bc9b84a8b98ede7d05c52662dd
anderson.f.vidal <anderson.f.vidal@accenture.com>
2020-09-20 16:33:22 -0300

    Labels

---
+++ src/components/atoms/Label/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { colors } from '../../../styles/colors';
+import 'antd/dist/antd.css';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
+export const LabelAton = styled(Title)`
+    
+`;
