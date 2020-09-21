469acc74ece7cbdb6261c27d271e1a3c13078413
Silva <sheilla.da.silva@accenture.com>
2020-09-21 15:37:38 -0300

    adding storybook for Label, Text and Title

--- src/components/atoms/Label/index.js
+++ src/components/atoms/Label/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { LabelAtom } from './styled';
 
-export const Label = ({text, ...props}) => {
-  return <LabelAtom {...props}>{text}</LabelAtom>
+export const Label = ({...props}) => {
+  return <LabelAtom {...props}/>
 }
 
 export default Label;
