755c5178a0085c7e6d42d5beb69e8fafc97592b0
Silva <sheilla.da.silva@accenture.com>
2020-09-28 14:00:36 -0300

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,22 @@
 const {defaults} = require('jest-config');
-
 module.exports = {
-    verbose: true,
-    moduleFileExtensions: [...defaults.moduleFileExtensions, 'js'],
+    moduleFileExtensions: ['js'],
+    testMatch: ["(/.*|(\\.|/)(test))\\.js?$"],
+    collectCoverageFrom: [
+        "**/*.{js,jsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**"
+    ], 
+    coverageThreshold: {
+        "jest": {
+            "coverageThreshold": {
+                "global": {
+                "branches": 80,
+                "functions": 80,
+                "lines": 80,
+                "statements": -10
+                }
+            }
+        }
+    },
 };
